3a6a867b2a53a1c9df1cdc3181056bb0748925ca
MchCyLh <MchCylH@gmail.com>
2014-11-20 07:31:35 +0800

    second edition

--- 13331163/s3/main.js
+++ 13331163/s3/main.js
@@ -1,3 +1,6 @@
+//  used for changed the state of all "li" and "button"
+//  all statues can be reset normally after all operator over
+
 function changeBackground(aLi, index, status) {
 	if (status == 0) {
 		for (var i = 0; i < aLi.length; i++) {
@@ -18,26 +21,112 @@ function changeBackground(aLi, index, status) {
 	}
 }
 
+
+//  for ajax , learn online
+var XMLHttp = { 
+    _objPool: [], 
+
+    _getInstance: function () 
+    { 
+        for (var i = 0; i < this._objPool.length; i ++) 
+        { 
+            if (this._objPool[i].readyState == 0 || this._objPool[i].readyState == 4) 
+            { 
+                return this._objPool[i]; 
+            } 
+        } 
+
+        // IE5中不支持push方法 
+        this._objPool[this._objPool.length] = this._createObj(); 
+
+        return this._objPool[this._objPool.length - 1]; 
+    }, 
+
+    _createObj: function () 
+    { 
+        if (window.XMLHttpRequest) 
+        { 
+            var objXMLHttp = new XMLHttpRequest(); 
+
+        } 
+        else 
+        { 
+            var MSXML = ['MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0', 'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP']; 
+            for(var n = 0; n < MSXML.length; n ++) 
+            { 
+                try 
+                { 
+                    var objXMLHttp = new ActiveXObject(MSXML[n]); 
+                    break; 
+                } 
+                catch(e) 
+                { 
+                } 
+            } 
+         }           
+
+        // mozilla某些版本没有readyState属性 
+        if (objXMLHttp.readyState == null) 
+        { 
+            objXMLHttp.readyState = 0; 
+
+            objXMLHttp.addEventListener("load", function () 
+                { 
+                    objXMLHttp.readyState = 4; 
+
+                    if (typeof objXMLHttp.onreadystatechange == "function") 
+                    { 
+                        objXMLHttp.onreadystatechange(); 
+                    } 
+                },  false); 
+        } 
+
+        return objXMLHttp; 
+    }, 
+
+    // 发送请求(方法[post,get], 地址, 数据, 回调函数) 
+    sendReq: function (method, url, data, callback) 
+    { 
+        var objXMLHttp = this._getInstance(); 
+
+        with(objXMLHttp) 
+        { 
+            try 
+            { 
+                // 加随机数防止缓存 
+                if (url.indexOf("?") > 0) 
+                { 
+                    url += "&randnum=" + Math.random(); 
+                } 
+                else 
+                { 
+                    url += "?randnum=" + Math.random(); 
+                } 
+
+                open(method, url, true); 
+
+                // 设定请求编码方式 
+                setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'); 
+                send(data); 
+                onreadystatechange = function () 
+                { 
+                    if (objXMLHttp.readyState == 4 && (objXMLHttp.status == 200 || objXMLHttp.status == 304)) 
+                    { 
+
+                        callback(objXMLHttp.responseText); 
+                    } 
+                } 
+            }
+            catch(e) 
+            {
+                alert(e); 
+            }
+        }
+    }
+};
+
 function ajax(url, successfunction, failfunction) {
-	var oAjax;
-	// 创建Ajax对象
-	if (window.XMLHttpRequest) {
-		oAjax = new XMLHttpRequest();
-	} else {
-		oAjax = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	// 连接服务器
-	oAjax.open("GET", url, true);
-	// 发送请求
-	oAjax.send();
-	// 接受返回
-	oAjax.onreadystatechange = function () {
-		if (oAjax.readyState == 4 && oAjax.status == 200) {
-			successfunction(oAjax.responseText);
-		} else {
-			// failfunction();
-		}
-	}
+	XMLHttp.sendReq('GET', url, '', successfunction);
 }
 
 window.onload = function () {
@@ -50,6 +139,8 @@ window.onload = function () {
 	var oExtend = document.getElementById("extend");
 	var oButton = document.getElementById("button");
 	var flag;
+
+	// for mouse out @+
 	for (var i = 0; i < aLi.length; i++) {
 		aLi[i].onmouseover = function () {
 			flag = 1;
@@ -86,6 +177,7 @@ window.onload = function () {
 		}, 0);
 	}
 
+	// initial
 	changeBackground(aLi, aLi.length+1, 1);
 
 
@@ -109,6 +201,7 @@ window.onload = function () {
 		}
 	}
 
+	// for click event
 	for (var i = 0; i < aLi.length; i++) {
 		aLi[i].clicked = 0;
 		aLi[i].onclick = function(i) {
@@ -116,8 +209,7 @@ window.onload = function () {
 				var oEvent = ev || event;
 				oEvent.cancelBubble = true;
 				if (this.style.backgroundColor == "gray") return;
-				// 改变背景颜色和span的内容
-				// if (aSpan[i].innerHTML == "") totalNum++;
+
 				var that = this;
 				aSpan[i].style.display = "block";
 				aSpan[i].innerHTML = "...";
